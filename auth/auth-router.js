6d6248acd1eddfdacb99be07ad21a83b2d2c24db
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-08 15:06:32 -0400

    Forgot to make small commits; maybe I'll remember tomorrow

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +27,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +40,17 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // what the token is describing
+    username: user.username,
+    roles: ['student'], // faking it
+  }
+  const options = {
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,16 @@
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
-  } else {
-    res.status(400).json({ message: 'No credentials provided' });
-  }
+  jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+    if(err) { // token is invalid/expired
+      res.status(401).json({ you: 'shall not pass!!!' })
+    } else { // token is valid
+      req.decodedToken = decodedToken;
+      next();
+    }
+  })
 };
